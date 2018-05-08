9536060c80272293877b9a0823609c097bc8e288
Toreno <kostet499@gmail.com>
2018-05-08 22:32:27 +0300

    added function that gets answer id list

--- stf_parser.py
+++ stf_parser.py
@@ -3,12 +3,14 @@ import re
 
 
 def get_html_doc(url):
+    """Open the url and return a string"""
     response = urllib.request.urlopen(url)
     html = str(response.read())
     return html
 
 
 def get_answer_quantity(html):
+    """"Extract the quantity of answers"""
     quest_sum = re.search(r'<h2 data-answercount="[0-9]*">', html)
     try:
         quest_sum = quest_sum.group(0)
@@ -18,5 +20,10 @@ def get_answer_quantity(html):
     return quest_sum
 
 
-print(get_answer_quantity
-      (get_html_doc('https://ru.stackoverflow.com/questions/825215/')))
+def get_answer_id_list(html):
+    """Get all answer IDs and return a list of them"""
+    matcher = re.findall(r'data-answerid="[0-9]*"', html)
+    id_list = list()
+    for group in matcher:
+        id_list.append(int(re.sub(r'[^0-9]', '', group)))
+    return id_list

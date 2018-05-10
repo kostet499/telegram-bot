2f8ceb5ec7e020594f84eef9766a858d3f355437
Toreno <kostet499@gmail.com>
2018-05-11 01:24:32 +0300

    Work done. At first look

--- database_script.py
+++ database_script.py
@@ -52,27 +52,90 @@ def check_user_to_be_in_db(username):
     return False
 
 
-def extract_last_answer_count(user_id, question_id):
-    """Extract last answer count of the question from database and update it"""
+def get_user_id(username):
+    conn = sqlite3.connect('stackquestion.db')
+    query = "SELECT id FROM users WHERE name = (\"%s\")" % username
+    cur = conn.cursor()
+    cur.execute(query)
+    user_id = cur.fetchone()
+    conn.close()
+    return user_id
+
+
+def compare_answers(user_id, question_id, answer_count):
+    """Compare last answer count with new
+     of the question from database
+     and update it"""
+
     conn = sqlite3.connect('stackquestion.db')
     cur = conn.cursor()
 
-    query = '''SELECT ans_number FROM users
-              WHERE user_id = (\"%s\") and
-              question_id  =(\"%s\") ''' % (user_id, question_id)
+    query = '''SELECT ans_number FROM user_question
+              WHERE user_id = %d and
+              question_id  = %d ''' % (user_id, question_id)
 
     answer = cur.execute(query).fetchone()
+    if answer is not None and answer == answer_count:
+        return True
     if answer is None:
-        answer = 0
         query = """INSERT INTO user_question(user_id, question_id, ans_number)
-                  VALUES ((\"%s\"), (\"%s\"), 0);""" % (user_id, question_id)
+                  VALUES ( %d, %d , %d);""" \
+                % (user_id, question_id, answer_count)
     else:
-        query = """UPDATE user_question SET ans_number = (\"%s\")
-                      WHERE user_id = (\"%s\") and question_id = (\"%s\")
-                   """ % (user_id, question_id, answer)
+        query = """UPDATE user_question SET ans_number = %d
+                      WHERE user_id = %d and question_id = %d
+                   """ % (user_id, question_id, answer_count)
 
     cur.execute(query)
     conn.commit()
+    conn.close()
+    return False
+
+
+def add_question(question_id):
+    conn = sqlite3.connect('stackquestion.db')
+    cur = conn.cursor()
+    query = "INSERT INTO questions(id) VALUES (%d)" % question_id
+    try:
+        cur.execute(query)
+        conn.commit()
+    except Exception:
+        pass
+    conn.close()
+
+
+def delete_question(user_id, question_id):
+    conn = sqlite3.connect('stackquestion.db')
+    cur = conn.cursor()
+
+    query = '''DELETE FROM user_question
+                  WHERE user_id = %d and
+                  question_id  = %d ''' % (user_id, question_id)
+    try:
+        cur.execute(query)
+        conn.commit()
+    except Exception:
+        conn.close()
+
+
+def get_all_user_names():
+    conn = sqlite3.connect('stackquestion.db')
+    cur = conn.cursor()
+
+    query = "SELECT name FROM users"
+    answer = list(cur.execute(query).fetchall())
+    conn.close()
+    return answer
+
+
+def get_question_by_user_id(user_id):
+    conn = sqlite3.connect('stackquestion.db')
+    cur = conn.cursor()
+
+    query = """SELECT question_id from user_question
+            WHERE user_id = %d
+            """ % user_id
+    answer = set(cur.execute(query).fetchall())
     conn.close()
     return answer
 
--- simple_bot.py
+++ simple_bot.py
@@ -1,6 +1,9 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import stf_parser
 import database_script
+import re
+
+monitoring_list = dict()
 
 
 def start(bot, update):
@@ -20,7 +23,54 @@ def count_answers(bot, update, args):
     update.message.reply_text(answer)
 
 
-def callback_check_question
+def form_link(question_id):
+    return "https://stackoverflow.com/questions/" + str(question_id) + '/'
+
+
+def callback_check_question(bot, job):
+    for name, questions in monitoring_list.items():
+        user_id = database_script.get_user_id(name)
+        for question_id in questions:
+            new_answer_quantity = \
+                stf_parser.get_answer_quantity(form_link(question_id))
+            if database_script.compare_answers(user_id,
+                                               question_id,
+                                               new_answer_quantity):
+                continue
+
+            text_message = "New answers \n(\"%s\")" \
+                           % (form_link(question_id))
+        bot.send_message(chat_id='@' + name, text=text_message)
+
+
+def add_question(bot, update, args):
+    question_id = 0
+    try:
+        question_id = int(args[0])
+    except Exception:
+        question_id = int(re.search(r'/[^0-9]*/', args[0]).group(0))
+    user_name = update.effective_user.username
+    monitoring_list[user_name].add(question_id)
+    database_script.add_question(question_id)
+
+
+def del_question(bot, update, args):
+    """Delete the question from monitoring list"""
+    question_id = 0
+    try:
+        question_id = int(args[0])
+    except Exception:
+        question_id = int(re.search(r'/[^0-9]*/', args[0]).group(0))
+    try:
+        user_name = update.effective_user.username
+        monitoring_list[user_name].remove(question_id)
+        user_id = database_script.get_user_id(user_name)
+        database_script.delete_question(user_id, question_id)
+        bot.send_message(chat_id=update.message.chat_id,
+                         text="Successfully deleted question")
+    except Exception:
+        bot.send_message(chat_id=update.message.chat_id,
+                         text="Sorry, no question to delete")
 
 
 def unknown(bot, update):
@@ -28,15 +78,31 @@ def unknown(bot, update):
                      text='It is wrong command')
 
 
+def fill_monitoring_list():
+    """Fill monitoring list"""
+    names = database_script.get_all_user_names()
+    for name in names:
+        monitoring_list[name] = database_script.get_question_by_user_id(name)
+
+
 def main():
     updater = Updater(open('token').read())
     dp = updater.dispatcher
+    fill_monitoring_list()
 
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler('count_answers',
                                   count_answers, pass_args=True))
+    dp.add_handler(CommandHandler('add_question',
+                                  add_question, pass_args=True))
+    dp.add_handler(CommandHandler('del_question',
+                                  del_question, pass_args=True))
     dp.add_handler(MessageHandler(Filters.command, unknown))
+
+    updater.job_queue.run_repeating(callback_check_question,
+                                    interval=60,
+                                    first=0)
     updater.start_polling()
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,

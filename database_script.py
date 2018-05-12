1abc8fe05950d7fbca163aea9ea0dc480ad7e141
Toreno <kostet499@gmail.com>
2018-05-12 22:55:59 +0300

    Sqlite3 global variables deleted

--- database_script.py
+++ database_script.py
@@ -1,15 +1,22 @@
 import sqlite3
 
-def create_user_table(conn):
+
+def create_user_table():
+    conn = sqlite3.connect('stackquestion.db')
+    cur = conn.cursor()
     cur.execute('''
         CREATE TABLE IF NOT EXISTS users (
             id INT NOT NULL PRIMARY KEY,
             name VARCHAR(255)
         );
     ''')
+    conn.commit()
+    conn.close()
 
 
-def create_rel_table(conn):
+def create_rel_table():
+    conn = sqlite3.connect('stackquestion.db')
+    cur = conn.cursor()
     cur.execute('''
           CREATE TABLE IF NOT EXISTS user_question(
               user_id INT NOT NULL,
@@ -20,32 +27,46 @@ def create_rel_table(conn):
               PRIMARY KEY(user_id, question_id)
               );
       ''')
+    conn.commit()
+    conn.close()
 
 
-def create_question_table(conn):
+def create_question_table():
+    conn = sqlite3.connect('stackquestion.db')
+    cur = conn.cursor()
     cur.execute('''
               CREATE TABLE IF NOT EXISTS questions(
                 id INT NOT NULL PRIMARY KEY
               );
           ''')
+    conn.commit()
+    conn.close()
 
 
 def check_user_to_be_in_db(chat_id, username):
     """Add user to database if he is new"""
+    conn = sqlite3.connect('stackquestion.db')
+    cur = conn.cursor()
     try:
         query = "INSERT INTO users(id, name) VALUES ({0}, \"{1}\");".format(
             chat_id,
             username)
         cur.execute(query)
         conn.commit()
+        conn.close()
         return True
     except Exception:
         return False
 
 
 def get_user_id(username):
+    conn = sqlite3.connect('stackquestion.db')
+    cur = conn.cursor()
     query = "SELECT id FROM users WHERE name = (\"%s\");" % username
-    return cur.execute(query).fetchone()
+    cur.execute(query)
+    answer = cur.fetchone()[0]
+    conn.close()
+    return answer
     
 
 def compare_answers(user_id, question_id, answer_count):
@@ -74,6 +95,8 @@ def compare_answers(user_id, question_id, answer_count):
 
 
 def insert_into_user_question(user_id, question_id, answer_count):
+    conn = sqlite3.connect('stackquestion.db')
+    cur = conn.cursor()
     query = """INSERT INTO user_question(user_id, question_id, ans_number)
                          VALUES ( {0}, {1} , {2});""".format(user_id,
                                                              question_id,
@@ -81,40 +104,52 @@ def insert_into_user_question(user_id, question_id, answer_count):
     try:
         cur.execute(query)
         conn.commit()
+        conn.close()
     except Exception:
         return
 
 
 def add_question(question_id):
+    conn = sqlite3.connect('stackquestion.db')
+    cur = conn.cursor()
     query = "INSERT INTO questions(id) VALUES ({0});".format(question_id)
     try:
         cur.execute(query)
         conn.commit()
+        conn.close()
     except Exception:
         return
 
 
 def delete_question(user_id, question_id):
+    conn = sqlite3.connect('stackquestion.db')
+    cur = conn.cursor()
     query = '''DELETE FROM user_question
                   WHERE user_id = {0} and
                   question_id  = {1};'''.format(user_id, question_id)
     try:
         cur.execute(query)
         conn.commit()
+        conn.close()
     except Exception:
         return
 
 
 def get_all_user_id():
+    conn = sqlite3.connect('stackquestion.db')
+    cur = conn.cursor()
     query = "SELECT id FROM users;"
     cur.execute(query)
     answer = list()
     for x in cur.fetchall():
         answer.append(x[0])
+    conn.close()
     return list(answer)
 
 
 def get_question_by_user_id(user_id):
+    conn = sqlite3.connect('stackquestion.db')
+    cur = conn.cursor()
     query = """SELECT question_id from user_question
             WHERE user_id = {};
             """.format(user_id)
@@ -122,16 +157,10 @@ def get_question_by_user_id(user_id):
     answer = list()
     for x in cur.fetchall():
         answer.append(x[0])
+    conn.close()
     return answer
 
 
-def close_database():
-    conn.close()
-
-
-conn = sqlite3.connect('stackquestion.db')
-cur = conn.cursor()
-create_question_table(conn)
-create_user_table(conn)
-create_rel_table(conn)
-conn.commit()
+create_question_table()
+create_user_table()
+create_rel_table()
